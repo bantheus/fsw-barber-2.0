272b11c70c6f32f8733e42ddf0a83e3b8aef9521
Matheus <matheuschmidt.b@gmail.com>
2024-08-07 14:39:18 -0300

    feat: add booking item

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,16 +5,16 @@ import { Card, CardContent } from "./ui/card"
 
 const Header = () => {
   return (
-    <Card>
+    <Card className="rounded-none border-b border-none">
       <CardContent className="flex items-center justify-between p-5">
         <Image
           src="/logo.png"
           alt="logo fsw barber"
-          width={130}
+          width={128}
           height={22}
           priority
           quality={100}
-          className="h-auto w-[130px] object-contain"
+          className="h-auto w-32 object-contain"
         />
 
         <Button size="icon" variant="ghost">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import Header from "./_components/header"
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
 
 export default function Home() {
@@ -11,7 +14,7 @@ export default function Home() {
 
       <div className="p-5">
         <h2 className="text-xl">
-          Olá, <span className="font-bold">Matheus!</span>{" "}
+          Olá, <span className="font-bold">Matheus!</span>
         </h2>
         <p className="text-sm">Terça-feira, 06 de agosto.</p>
 
@@ -22,15 +25,41 @@ export default function Home() {
           </Button>
         </div>
 
-        <div className="relative mt-6 h-[200px] w-full">
+        <div className="relative mt-2 h-48 w-full">
           <Image
             src="/banner.svg"
             alt="agende nos melhores com fsw barber"
             fill
             quality={100}
-            className="rounded-xl object-cover"
+            className="rounded-xl object-contain"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="size-6">
+                  <AvatarImage src="https://utfs.io/f/178da6b6-6f9a-424a-be9d-a2feb476eb36-16t.png" />
+                  <AvatarFallback>MS</AvatarFallback>
+                </Avatar>
+
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* direira */}
+            <div className="flex flex-col items-center justify-center gap-2 border-l border-solid px-5 py-5">
+              <p className="text-sm">agosto</p>
+              <p className="text-2xl">07</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   )
